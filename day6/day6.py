3e11ffdefc0f846216cfc7bbdcf8485223046d80
Katie Mata <kmata@phmgmt.com>
2020-12-06 11:27:47 -0600

    Add day6 Changes to be committed: 	modified:   day6.py 	modified:   day6_1.txt

--- day6/day6.py
+++ day6/day6.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 
+from functools import reduce
+
 def read_input_file(filename):
     with open(filename, 'r') as f:
-        return f.read().splitlines() # removes '/n'
+        return f.read().strip()
 
-lines = read_input_file('day6_1.txt')
-print(lines)
+def create_sets(input):
+    return [{char for form in group.split('\n') for char in form} for group in input.split('\n\n')]
+
+def create_intersections(input):
+    intersections = []
+    for group in input.split('\n\n'):
+        sets = [{char for char in form} for form in group.split('\n')]
+        head, *tail = sets
+        intersections.append(head.intersection(*tail))
+
+    return intersections
+
+input = read_input_file('day6_1.txt')
+sets = create_sets(input)
+print(sum([len(set) for set in sets]))
+
+intersections = create_intersections(input)
+print(sum([len(intersection) for intersection in intersections]))
